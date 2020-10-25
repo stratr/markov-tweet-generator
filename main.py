f458cec3215d934848406bc2624830e5cdce0f08
Taneli <taneli.salonen@gmail.com>
2020-10-25 16:54:13 +0000

    fix bug when walk graph goes to a dead end

--- main.py
+++ main.py
@@ -6,7 +6,30 @@ from google.cloud import bigquery
 
 client = bigquery.Client()
 
-markov_graph = defaultdict(lambda: defaultdict(int))
+markov_graph = defaultdict(lambda: defaultdict(lambda: [0, True]))
+
+# test_dict = defaultdict(lambda: defaultdict(lambda: [0, True]))
+
+# test_dict['numbers']['test'] = [1, False]
+# test_dict['numbers']['asdf'] = [1, False]
+# #print(test_dict['numbers']['dfdfdf'])
+
+# print('keys:')
+# print(test_dict['numbers'].keys())
+
+# print('values:')
+# print(test_dict['numbers'].values())
+
+# print('testing mapped values:')
+# for value in test_dict['numbers'].values():
+#     print(value[0])
+
+# print(list(map(lambda x: x[0], test_dict['numbers'].values())))
+
+
+# sadfsdfsdffsd
+
+
 
 def get_markov_graph():
     data = []
@@ -33,59 +56,14 @@ def get_markov_graph():
         for obj in next_array:
             next_word = obj["word"]
             next_word_count = obj["count"]
-            markov_graph[current_word][next_word] = next_word_count
+            next_word_weight = obj["weight"]
+            next_word_stop = obj["stop"] # if the word is a "stop word" that leads to a dead end
+            markov_graph[current_word][next_word] = [next_word_weight, next_word_stop]
 
 
 get_markov_graph()
-#print(markov_graph)
 
 print('graph created')
-# asfdfd
-
-
-# def get_text_array():
-#     data = []
-
-#     query_sql = """
-#     SELECT
-#     word
-#     FROM
-#     `tanelis.tweets_eu.tweets_reporting`,
-#     UNNEST(words) AS word
-#     LIMIT 1000
-#     """
-
-#     client = bigquery.Client()
-#     query_job = client.query(query_sql)
-#     results = query_job.result()
-
-#     for row in results:
-#         data.append(row["word"])
-
-#     return data
-
-
-# tokenized_text = get_text_array()
-
-# # print(tokenized_text)
-
-# last_word = tokenized_text[0].lower()
-# for word in tokenized_text[1:]:
-#     word = word.lower()
-#     markov_graph[last_word][word] += 1
-#     last_word = word
-
-# print(markov_graph)
-
-
-
-# Preview graph.
-# limit = 3
-# for first_word in ('Vake', 'palkkasi'):
-#   next_words = list(markov_graph[first_word].keys())[:limit]
-#   for next_word in next_words:
-#     print(first_word, next_word)
-
 
 # TODO:
 # Select the start word randomly based what have actually been common start words
@@ -96,7 +74,10 @@ print('graph created')
 # time to stop. E.g. based on how often the word has ended a tweet. The longer the tweet
 # is the more likely the word should be to end it.
 
-def walk_graph(graph, distance=5, start_node=None):
+# graph = the markov graph as a dictionary
+# distance = the number of words per tweet
+# start_node = the word to start with
+def walk_graph(graph, distance=8, start_node=None):
     """Returns a list of words from a randomly weighted walk."""
     if distance <= 0:
         return []
@@ -105,21 +86,25 @@ def walk_graph(graph, distance=5, start_node=None):
     if not start_node:
         start_node = random.choice(list(graph.keys()))
 
-    weights = np.array(
-        list(markov_graph[start_node].values()),
-        dtype=np.float64)
-    # Normalize word counts to sum to 1.
-    weights /= weights.sum()
-
     # Pick a destination using weighted distribution.
     choices = list(markov_graph[start_node].keys())
+    weights = list(map(lambda x: x[0], markov_graph[start_node].values()))
     chosen_word = np.random.choice(choices, None, p=weights)
+
+    # Check if the chosen word leads to a dead end. Only allowed if min length for tweet has been reached.
+    if markov_graph[start_node][chosen_word][1] == True:
+        print('is a stop word, should not be used ' + chosen_word)
+        # the word is skipped
+        return walk_graph(
+            graph, distance=distance,
+            start_node=start_node)
 
     return [chosen_word] + walk_graph(
         graph, distance=distance-1,
         start_node=chosen_word)
 
-
+# Print tweets
+start_word = 'tämä'
 for i in range(10):
-    print(' '.join(walk_graph(
-        markov_graph, distance=12, start_node="tämä")), '\n')
+    print(start_word + ' ' + ' '.join(walk_graph(
+        markov_graph, distance=12, start_node=start_word)), '\n')

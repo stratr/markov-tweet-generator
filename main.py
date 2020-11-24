1016258850e90d67e9d75f664b49763de373d72d
Taneli <taneli.salonen@gmail.com>
2020-11-24 16:21:21 +0000

    load data from Storage

--- main.py
+++ main.py
@@ -1,45 +1,31 @@
 import numpy as np
 import random
-#import re
+import pickle
 from collections import defaultdict
+from google.cloud import storage
 from google.cloud import bigquery
 
-client = bigquery.Client()
 
-# initiate markov graph dictionary
-markov_graph = defaultdict(lambda: defaultdict(lambda: [0, True]))
+storage_client = storage.Client()
+bucket = storage_client.bucket('markov_generator')
+folder = 'all_meps'
 
-# start word weights
-start_words = []
-start_weights = []
+def load_pickle(bucket, folder, file_name):
+    # Load a pickled file and return a dictionary
 
-# build the ditionary and get start word weights
-def get_source_data():
-    # The source data is already prepared in BigQuery. Read it into dictionary format.
-    query_sql = "SELECT * FROM `tanelis.markov_chain.markov_graph`"
+    file_path = '{}/{}.pickle'.format(folder, file_name)
+    print('Loading file: {}'.format(file_path))
+    blob = bucket.blob(file_path)
+    
+    return pickle.loads(blob.download_as_string())
 
-    client = bigquery.Client()
-    query_job = client.query(query_sql)
-    results = query_job.result()
+# Load the markov graph
+markov_graph = load_pickle(bucket, folder, 'markov_graph')
+# print(markov_graph['ja']['suru'][1])
+# asdfdfsdfs
 
-    for row in results:
-        current_word = row["current_word"]
-        start_weight = row["start_probability"]
-        next_array = row["next"]
-
-        # Get the next words data for the markov graph
-        for obj in next_array:
-            next_word = obj["word"]
-            next_word_weight = obj["weight"]
-            next_word_stop = obj["stop"] # if the word is a "stop word" that leads to a dead end
-            next_word_end_probability = obj["end_probability"] # a probability for each word on how often they should end the tweet once min words are reached
-
-            markov_graph[current_word][next_word] = [
-                next_word_weight, next_word_stop, next_word_end_probability]
-
-        # Get the start word weights for when the tweet is generated with a random start word
-        start_words.append(current_word)
-        start_weights.append(start_weight)
+# Load start words
+start_words = load_pickle(bucket, folder, 'start_words')
 
 
 def generate_tweet(graph, min_distance=5, max_distance=8, start_word=None):
@@ -50,7 +36,9 @@ def generate_tweet(graph, min_distance=5, max_distance=8, start_word=None):
     start_word is the first word the the generator uses to start iterating through the markov graph. If not specified a random wrod will be used.
     """
 
+    # Tweets have an increasing probability of ending after the min number of words has been achieved
     num_end_tries = max_distance - min_distance
+    end_try = 1 
 
     if not start_word:
         start_word = random.choice(list(graph.keys()))
@@ -58,76 +46,78 @@ def generate_tweet(graph, min_distance=5, max_distance=8, start_word=None):
     tweet_words = [start_word]
     current_word = start_word
 
-    i = max_distance
+    i = max_distance - 1 # the start word has already been added
     while i > 0:
         # Pick the next word based on the weights and current word
-        choices = list(markov_graph[start_word].keys())
-        weights = list(map(lambda x: x[0], markov_graph[start_word].values()))
+        choices = list(markov_graph[current_word].keys())
+        weights = list(map(lambda x: x[0], markov_graph[current_word].values()))
         next_word = np.random.choice(choices, None, p=weights)
 
         # Check if the chosen word is a stop word (dead end)
         if markov_graph[current_word][next_word][1] == True:
             if len(tweet_words) >= min_distance:
                 # End the tweet with the chosen word
+                print('Ending the word because of a stop word')
                 tweet_words.append(next_word)
-                current_word = next_word
                 break
             else:
                 # Try to pick another word
                 continue
 
-
         # If min tweet length has been already reached take a flip to see if the chosen word will be the last one
-        # TODO: still a work in progress
-        # make sure that current_word and next_word are being used correctly
         if len(tweet_words) >= min_distance:
-            end_probability = markov_graph[current_word][next_word][2] * ((min_distance-1)*-1/end_tries)
+            end_probability = markov_graph[current_word][next_word][2] * end_try / num_end_tries
+            end_try += 1
+            if np.random.random_sample() <= end_probability:
+                print('end the tweet with end probability: ' + str(end_probability))
+                tweet_words.append(next_word)
+                break
 
         tweet_words.append(next_word)
         current_word = next_word
         i -= 1
-    else:
-        return ' '.join(tweet_words).capitalize()
+    
+    return ' '.join(tweet_words).capitalize()
 
 
-#TODO: transform this function into a loop (iterative) insteadof the recursive format. That way it shouldn't run into recursion limits.
-def walk_graph(graph, min_distance=5, max_distance=8, start_node=None, end_tries=0):
-    """Returns a list of words from a randomly weighted walk."""
-    if max_distance <= 0:
-        return []
+# #TODO: transform this function into a loop (iterative) insteadof the recursive format. That way it shouldn't run into recursion limits.
+# def walk_graph(graph, min_distance=5, max_distance=8, start_node=None, end_tries=0):
+#     """Returns a list of words from a randomly weighted walk."""
+#     if max_distance <= 0:
+#         return []
 
-    # If not given, pick a start node at random.
-    if not start_node:
-        start_node = random.choice(list(graph.keys()))
+#     # If not given, pick a start node at random.
+#     if not start_node:
+#         start_node = random.choice(list(graph.keys()))
 
-    # Pick a destination using weighted distribution.
-    choices = list(markov_graph[start_node].keys())
-    weights = list(map(lambda x: x[0], markov_graph[start_node].values()))
-    chosen_word = np.random.choice(choices, None, p=weights)
+#     # Pick a destination using weighted distribution.
+#     choices = list(markov_graph[start_node].keys())
+#     weights = list(map(lambda x: x[0], markov_graph[start_node].values()))
+#     chosen_word = np.random.choice(choices, None, p=weights)
 
-    # Check if the chosen word leads to a dead end. Only allowed if min length for tweet has been reached.
-    if markov_graph[start_node][chosen_word][1] == True:
-        # allow the tweet to end if the word is a "stop word" and min_distance has been reached
-        if min_distance <= 0:
-            #print('end the text in a stop word: ' + chosen_word)
-            return [chosen_word]
-        # skip the chosen word and retry
-        else:
-            #print('chosen word is a dead end, choose another one: ' + chosen_word)
-            return walk_graph(
-                graph, min_distance=min_distance, max_distance=max_distance,
-                start_node=start_node, end_tries=end_tries)
+#     # Check if the chosen word leads to a dead end. Only allowed if min length for tweet has been reached.
+#     if markov_graph[start_node][chosen_word][1] == True:
+#         # allow the tweet to end if the word is a "stop word" and min_distance has been reached
+#         if min_distance <= 0:
+#             #print('end the text in a stop word: ' + chosen_word)
+#             return [chosen_word]
+#         # skip the chosen word and retry
+#         else:
+#             #print('chosen word is a dead end, choose another one: ' + chosen_word)
+#             return walk_graph(
+#                 graph, min_distance=min_distance, max_distance=max_distance,
+#                 start_node=start_node, end_tries=end_tries)
 
-    # If min_distance is reached use an increasing probabibility and the word probability as a possibility to end the tweet before
-    if min_distance <= 0:
-        end_probability = markov_graph[start_node][chosen_word][2] * ((min_distance-1)*-1/end_tries)
-        if np.random.random_sample() <= end_probability:
-            #print('end the tweet with end probability: ' + str(end_probability))
-            return [chosen_word]
+#     # If min_distance is reached use an increasing probabibility and the word probability as a possibility to end the tweet before
+#     if min_distance <= 0:
+#         end_probability = markov_graph[start_node][chosen_word][2] * ((min_distance-1)*-1/end_tries)
+#         if np.random.random_sample() <= end_probability:
+#             #print('end the tweet with end probability: ' + str(end_probability))
+#             return [chosen_word]
 
-    return [chosen_word] + walk_graph(
-        graph, min_distance=min_distance-1, max_distance=max_distance-1,
-        start_node=chosen_word, end_tries=end_tries)
+#     return [chosen_word] + walk_graph(
+#         graph, min_distance=min_distance-1, max_distance=max_distance-1,
+#         start_node=chosen_word, end_tries=end_tries)
 
 
 def generateTweets(graph, min_distance=6, max_distance=16, start_node=None, number_of_tweets=10):
@@ -148,9 +138,20 @@ def generateTweets(graph, min_distance=6, max_distance=16, start_node=None, numb
     
     return generated_tweets
 
+
+def random_start_word(start_words):
+    choices = list(start_words.keys())
+    weights = list(start_words.values())
+    start_word = np.random.choice(choices, None, p=weights)
+
+    return start_word
+
+print(random_start_word(start_words))
+sdfdsfsdfsfdfsd
+
+
 # load the markov graph
-get_source_data()
-new_tweet = generate_tweet(markov_graph, min_distance=5, max_distance=8, start_word="ja")
+new_tweet = generate_tweet(markov_graph, min_distance=5, max_distance=16, start_word="ja")
 print(new_tweet)
 
 sadfasfdfsda

8f8569078ac8c708b0366191a2955ad00234bbd3
Taneli <taneli.salonen@gmail.com>
2020-11-28 15:49:14 +0000

    fix

--- main.py
+++ main.py
@@ -42,12 +42,12 @@ def generate_tweet(graph, min_distance=5, max_distance=8, start_word=None):
     i = max_distance - 1 # the start word has already been added
     while i > 0:
         # Pick the next word based on the weights and current word
-        choices = list(markov_graph[current_word].keys())
-        weights = list(map(lambda x: x[0], markov_graph[current_word].values()))
+        choices = list(graph[current_word].keys())
+        weights = list(map(lambda x: x[0], graph[current_word].values()))
         next_word = np.random.choice(choices, None, p=weights)
 
         # Check if the chosen word is a stop word (dead end)
-        if markov_graph[current_word][next_word][1] == True:
+        if graph[current_word][next_word][1] == True:
             if len(tweet_words) >= min_distance:
                 # End the tweet with the chosen word
                 #print('Ending the word because of a stop word')
@@ -59,7 +59,7 @@ def generate_tweet(graph, min_distance=5, max_distance=8, start_word=None):
 
         # If min tweet length has been already reached take a flip to see if the chosen word will be the last one
         if len(tweet_words) >= min_distance:
-            end_probability = markov_graph[current_word][next_word][2] * end_try / num_end_tries
+            end_probability = graph[current_word][next_word][2] * end_try / num_end_tries
             end_try += 1
             if np.random.random_sample() <= end_probability:
                 #print('end the tweet with end probability: ' + str(end_probability))
@@ -87,7 +87,7 @@ def random_start_words(start_words, word_count):
     return words
 
 
-def generate_new_tweets(number_tweets):
+def generate_new_tweets(number_tweets, markov_graph, start_words):
     # Generate x number of start words based on start weights
     starts = random_start_words(start_words, number_tweets)
 
@@ -105,12 +105,10 @@ def generate_new_tweets(number_tweets):
             })
 
     # Insert the generated tweets into bigquery
-    #rows_to_insert = map(lambda x: {u"text": x}, tweets)
-    rows_to_insert = tweets
     table_id = 'tanelis.markov_chain.generated_tweets'
     client = bigquery.Client()
 
-    errors = client.insert_rows_json(table_id, rows_to_insert)
+    errors = client.insert_rows_json(table_id, tweets)
     if errors == []:
         print("New rows have been added.")
     else:
@@ -120,7 +118,7 @@ def generate_new_tweets(number_tweets):
 def create_new_tweets(event, context):
     markov_graph = load_pickle(bucket, folder, 'markov_graph')
     start_words = load_pickle(bucket, folder, 'start_words')
-    generate_new_tweets(100)
+    generate_new_tweets(100, markov_graph, start_words)
 
-    pubsub_message = base64.b64decode(event['data']).decode('utf-8')
-    print(pubsub_message)
+
+create_new_tweets(None, None)
